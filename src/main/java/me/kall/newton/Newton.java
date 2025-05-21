fc4d551737564351113575230aac04618b8ff4cb
狂想ThePotato <670067575@qq.com>
2025-05-21 23:03:46 +0800

    port to 1215neoforge

--- src/main/java/me/kall/newton/Newton.java
+++ src/main/java/me/kall/newton/Newton.java
@@ -1,10 +1,10 @@
 package me.kall.newton;
 
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,15 +16,15 @@ public class Newton {
     public static final Set<UUID> APPLES = new ObjectOpenHashSet<>();
     public static final Logger LOGGER = LogManager.getLogger("Newton");
 
-    public Newton() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CONFIG);
+    public Newton(ModContainer container) {
+        container.registerConfig(ModConfig.Type.COMMON, CONFIG);
     }
 
-    public static final ForgeConfigSpec CONFIG;
-    public static final ForgeConfigSpec.IntValue POSSIBILITY, MAX_COUNT;
+    public static final ModConfigSpec CONFIG;
+    public static final ModConfigSpec.IntValue POSSIBILITY, MAX_COUNT;
 
     static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
         builder.push("Newton");
         MAX_COUNT = builder.comment("Max apples that this mod will generate. Once this is reached, all generated apples in the world will be cleared and the generation and counting will be reset").defineInRange("MaxCount", 100, 0, Integer.MAX_VALUE);
         POSSIBILITY = builder.comment("Basic possibility for an apple to appear. Calculation: 1/TheNumberHere").defineInRange("PossibilityNumber", 1500, 0, Integer.MAX_VALUE);
--- src/main/java/me/kall/newton/mixin/MixinServerWorld.java
+++ src/main/java/me/kall/newton/mixin/MixinServerWorld.java
@@ -1,57 +1,54 @@
 package me.kall.newton.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import me.kall.newton.Newton;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LeavesBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.item.Items;
-import net.minecraft.profiler.IProfiler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkSection;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraft.world.storage.ISpawnWorldInfo;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WritableLevelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Supplier;
 
 @ParametersAreNonnullByDefault
-@Mixin(ServerWorld.class)
-public abstract class MixinServerWorld extends World {
-    protected MixinServerWorld(ISpawnWorldInfo p_i241925_1_, RegistryKey<World> p_i241925_2_, DimensionType p_i241925_3_, Supplier<IProfiler> p_i241925_4_, boolean p_i241925_5_, boolean p_i241925_6_, long p_i241925_7_) {
-        super(p_i241925_1_, p_i241925_2_, p_i241925_3_, p_i241925_4_, p_i241925_5_, p_i241925_6_, p_i241925_7_);
+@Mixin(ServerLevel.class)
+public abstract class MixinServerWorld extends Level {
+
+    protected MixinServerWorld(WritableLevelData arg, ResourceKey<Level> arg2, RegistryAccess arg3, Holder<DimensionType> arg4, boolean bl, boolean bl2, long l, int i) {
+        super(arg, arg2, arg3, arg4, bl, bl2, l, i);
     }
 
-    @Shadow @Nullable public abstract Entity getEntity(UUID pUniqueId);
     @Shadow public abstract boolean addFreshEntity(Entity pEntity);
 
     @Unique private int newton$maxCount;
 
-    @Inject(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isRandomlyTicking()Z"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void newton$onTickBlock(Chunk pChunk, int pRandomTickSpeed, CallbackInfo ci, ChunkPos chunkpos, boolean flag, int i, int j, IProfiler iprofiler, ChunkSection[] var8, int var9, int var10, ChunkSection chunksection, int k, int l, BlockPos blockpos1, BlockState blockstate) {
-        if (ThreadLocalRandom.current().nextInt(Newton.POSSIBILITY.get()) == 1 && BlockTags.LEAVES.contains(blockstate.getBlock()) && !blockstate.getValue(LeavesBlock.PERSISTENT) && Objects.equals(this.getBlockState(blockpos1.below()), Blocks.AIR.defaultBlockState())) {
-            int x = blockpos1.getX();
-            int y = blockpos1.getY() - 1;
-            int z = blockpos1.getZ();
-            ItemEntity apple = new ItemEntity((ServerWorld)(Object)this, x, y, z, Items.APPLE.getDefaultInstance());
+    @Inject(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isRandomlyTicking()Z"))
+    private void newton$onTickBlock(LevelChunk levelChunk, int m, CallbackInfo ci, @Local BlockPos pos, @Local BlockState state) {
+        if (ThreadLocalRandom.current().nextInt(Newton.POSSIBILITY.get()) == 1 && state.is(BlockTags.LEAVES)  && !state.getValue(LeavesBlock.PERSISTENT) && Objects.equals(this.getBlockState(pos.below()), Blocks.AIR.defaultBlockState())) {
+            int x = pos.getX();
+            int y = pos.getY() - 1;
+            int z = pos.getZ();
+            ItemEntity apple = new ItemEntity((ServerLevel)(Object)this, x, y, z, Items.APPLE.getDefaultInstance());
             UUID id = this.newton$getUniqueId();
             apple.setUUID(id);
 
@@ -62,7 +59,7 @@ public abstract class MixinServerWorld extends World {
             if (this.newton$maxCount == Newton.MAX_COUNT.get()) {
                 for (UUID uuid : Newton.APPLES) {
                     Entity entity = this.getEntity(uuid);
-                    if (entity != null) entity.remove();
+                    if (entity != null) entity.remove(Entity.RemovalReason.KILLED);
                 }
                 Newton.APPLES.clear();
                 this.newton$maxCount = 0;
